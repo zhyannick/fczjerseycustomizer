e2027fd1061a6f0897fabfbeac9df7170cb084ed
Bytyqi Erson <Erson.Bytyqi@swisscom.com>
2022-07-07 14:01:34 +0200

    -
    -

--- pages/TrikotTrailer.js
+++ pages/TrikotTrailer.js
@@ -5,7 +5,7 @@ export default function TrikotTrailer() {
         <div className={styles.BackgroundImage}>
             <h1 className={styles.TrikotTrailerTitle}>Trikot Trailer</h1>
             <div className={styles.video}>
-                <iframe className={styles.iframe} src="https://www.youtube.com/embed/GVd9wGtt-ww" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe className={styles.iframe} src="https://www.youtube.com/embed/GVd9wGtt-ww" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
             </div>
         </div>
     )
